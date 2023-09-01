429bf76e6722d79f391f9a877d728fedab840fa9
vigneshholla <vigneshrao004@gmail.com>
2023-09-01 20:09:29 +0530

    added Subtraction function

--- python_progs/arith.py
+++ python_progs/arith.py
@@ -4,3 +4,10 @@ print('for addition')
 a=int(input("Enter num1:"))
 b=int(input("Enter num2:"))
 print("sum=",add(a,b))
+
+def sub(n1,n2):
+    return(n1+n2)
+print('for subtraction')
+a=int(input("Enter num1:"))
+b=int(input("Enter num2:"))
+print("sum=",sub(a,b))
